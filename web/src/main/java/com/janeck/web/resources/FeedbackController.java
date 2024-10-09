8959e7249f9790a70197897efafc37d64cb0836c
Janeck <Janeck.ing.tabors@gmail.com>
2024-10-09 09:23:46 +0200

    Add Feedback (fixed), add new css, add Security

--- web/src/main/java/com/janeck/web/resources/FeedbackController.java
+++ web/src/main/java/com/janeck/web/resources/FeedbackController.java
@@ -2,12 +2,16 @@ package com.janeck.web.resources;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.janeck.web.tools.Feedback;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,18 +23,11 @@ public class FeedbackController {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final File file = new File("C:\\Users\\jta\\Documents\\Projects\\web\\src\\main\\resources\\static\\feedback.json");
 
-    @PostMapping
-    public ResponseEntity<String> submitFeedback(@RequestBody Feedback feedback) throws IOException {
-        System.out.println("Empfangenes Feedback: " + feedback);
-        feedbackList.add(feedback);
-        System.out.println("Aktualisierte Feedback-Liste: " + feedbackList);
-        objectMapper.writeValue(file, feedbackList);
-        System.out.println("Feedback gespeichert: " + feedback);
-        return ResponseEntity.ok("Feedback submitted successfully");
+    public FeedbackController() {
+        loadFeedbackFromFile();
     }
 
-    @GetMapping("/all")
-    public ResponseEntity<List<Feedback>> getAllFeedback() {
+    private void loadFeedbackFromFile() {
         try {
             if (file.exists() && file.length() != 0) {
                 Feedback[] feedbackArray = objectMapper.readValue(file, Feedback[].class);
@@ -40,10 +37,31 @@ public class FeedbackController {
             } else {
                 System.out.println("Datei nicht gefunden oder leer: " + file.getAbsolutePath());
             }
-            return ResponseEntity.ok(feedbackList);
         } catch (IOException e) {
             e.printStackTrace();
-            return ResponseEntity.status(500).body(null);
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<String> submitFeedback(@RequestBody Feedback feedback) throws IOException {
+        System.out.println("Empfangenes Feedback: " + feedback);
+        feedbackList.add(feedback);
+        objectMapper.writeValue(file, feedbackList);
+        System.out.println("Feedback gespeichert: " + feedback);
+        return ResponseEntity.ok("Feedback erfolgreich eingereicht");
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Feedback>> getAllFeedback() {
+        return ResponseEntity.ok(feedbackList);
+    }
+
+    @GetMapping("/view")
+    public ResponseEntity<byte[]> getAalPage() throws IOException {
+        ClassPathResource resource = new ClassPathResource("static/feedback-view.html");
+        byte[] content = Files.readAllBytes(Path.of(resource.getURI()));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "text/html; charset=UTF-8");
+        return new ResponseEntity<>(content, headers, HttpStatus.OK);
     }
 }
